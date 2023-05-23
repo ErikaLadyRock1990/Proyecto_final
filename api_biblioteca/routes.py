1ab80cd3c7b80509d3353bc3eebd44c9f9e23556
Erika <kikayus@hotmail.com>
2023-05-23 17:31:56 +0200

    Clase cliente terminada

--- api_biblioteca/routes.py
+++ api_biblioteca/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from .models import *
 from .services.book_service import BookService
+from .services.client_service import ClientService
 
 bp = Blueprint("api_biblioteca", __name__)
 
@@ -73,12 +74,79 @@ def actualizar_libro():
     libro = BookService.actualizar_libro(id, titulo, autor, genero, año)
 
     libro_json = {
-            "id": libro.id,
-            "titulo": libro.titulo,
-            "autor": libro.autor,
-            "genero": libro.genero,
-            "año": libro.año,
-        }
-    
+        "id": libro.id,
+        "titulo": libro.titulo,
+        "autor": libro.autor,
+        "genero": libro.genero,
+        "año": libro.año,
+    }
 
     return jsonify(libro_json)
+
+
+@bp.route("/guardar-cliente", methods=["POST"])
+def guardar_cliente():
+    dni = request.json.get("dni")
+    nombre = request.json.get("nombre")
+    telefono = request.json.get("telefono")
+
+    ClientService.guardar_cliente(dni, nombre, telefono)
+
+    return "Cliente guardado correctamente", 201
+
+
+@bp.route("/buscar-cliente", methods=["GET"])
+def buscar_cliente():
+    dni = request.json.get("dni")
+    nombre = request.json.get("nombre")
+    telefono = request.json.get("telefono")
+
+    clientes = ClientService.buscar_clientes(dni=dni, nombre=nombre, telefono=telefono)
+
+    clientes_json = []
+    for cliente in clientes:
+        libro_json = {
+            "id": cliente.id,
+            "dni": cliente.dni,
+            "nombre": cliente.nombre,
+            "telefono": cliente.telefono,
+        }
+        clientes_json.append(libro_json)
+
+    # Puedes retornar los clientes en un formato adecuado, como JSON
+    return jsonify(clientes_json)
+
+
+@bp.route("/borrar-cliente", methods=["POST"])
+def borrar_cliente():
+    id = request.json.get("id")  # Obtener el valor del campo "id" del JSON
+    if not id:
+        return jsonify({"mensaje": "Cliente no encontrado"}), 400
+
+    # Intentar borrar el libro utilizando el ID
+    cliente_borrado = ClientService.borrar_cliente(id)
+
+    if cliente_borrado:
+        return jsonify({"mensaje": "Cliente borrado correctamente"}), 200
+    else:
+        return jsonify({"mensaje": "No se encontró el cliente"}), 404
+
+
+@bp.route("/actualizar-cliente", methods=["POST"])
+def actualizar_cliente():
+    id = request.json.get("id")
+    dni = request.json.get("dni")
+    nombre = request.json.get("nombre")
+    telefono = request.json.get("telefono")
+
+    cliente = ClientService.actualizar_cliente(id, dni=dni, nombre=nombre, telefono=telefono)
+   
+    cliente_json = {
+        "dni": cliente.dni,
+        "nombre": cliente.nombre,
+        "telefono": cliente.telefono
+        
+        
+    }
+
+    return jsonify(cliente_json)
---
+++ api_biblioteca/services/client_service.py
@@ -0,0 +1,55 @@
+from api_biblioteca.models import Cliente, db
+
+
+class ClientService:
+    @staticmethod
+    def guardar_cliente(dni, nombre, telefono):
+        nuevo_cliente = Cliente(dni=dni, nombre=nombre, telefono=telefono)
+        db.session.add(nuevo_cliente)
+        db.session.commit()
+
+    @staticmethod
+    def buscar_clientes(dni=None, nombre=None, telefono=None):
+        query = Cliente.query  # Inicializar la consulta con todos los clientes
+
+        if dni:
+            query = query.filter(Cliente.dni.ilike(f"%{dni}%"))
+        if nombre:
+            query = query.filter(Cliente.nombre.ilike(f"%{nombre}%"))
+        if telefono:
+            query = query.filter(Cliente.telefono == telefono)
+
+        clientes = query.all()
+
+        return clientes
+
+    @staticmethod
+    def borrar_cliente(id):
+        cliente = Cliente.query.get(id)  # Buscar el cliente por su ID
+
+        if cliente:
+            db.session.delete(cliente)  # Eliminar el cliente de la base de datos
+            db.session.commit()  # Confirmar los cambios en la base de datos
+
+            return True  # Indicar que el cliente se borró correctamente
+        else:
+            return False  # Indicar que el cliente no fue encontrado
+
+    @staticmethod
+    def actualizar_cliente(id, dni=None, nombre=None, telefono=None):
+        cliente = Cliente.query.get(id)  # Buscar el cliente por su ID
+
+        if not cliente:
+            return None
+        
+        if dni:
+            cliente.dni = dni
+        if nombre:
+            cliente.nombre = nombre
+        if telefono:
+            cliente.telefono = telefono
+
+        db.session.commit()  # Confirmar los cambios en la base de datos
+
+        return cliente  # Devolver el cliente actualizado
+    
